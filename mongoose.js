e0154744164f46a1b81c37fd87cc5e2d0c409a5a
umitsurumi <nobodycao@outlook.com>
2023-03-19 17:47:56 +0800

    vercel

--- mongoose.js
+++ mongoose.js
@@ -1,7 +1,8 @@
 // 连接远程mongodb数据库
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
-require("./config");
+// vercel部署不需要
+// require("./config");
 mongoose
   .connect(process.env.DATABASE_URL)
   .then(() => console.log("连接成功"))
